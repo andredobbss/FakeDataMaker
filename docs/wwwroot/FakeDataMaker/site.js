d3c50fd06a110796c063538ab3c978cd87378615
André dos Santos Dobbss de Carvalho <andredobbss@hotmail.com>
2025-05-19 12:52:57 -0300

    Corrigindo estrutura para GitHub Pages

--- docs/wwwroot/FakeDataMaker/site.js
+++
@@ -1,8 +0,0 @@
-﻿window.onBeforeUnloadHandler = {
-    attachUnloadListener: function () {
-        window.addEventListener('beforeunload', function () {
-            localStorage.removeItem('selectedLanguage');
-            localStorage.removeItem('quantity');
-        });
-    }
-};
