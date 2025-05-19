bcfe3935ddce713fed14fa6954a92800d323eb54
André dos Santos Dobbss de Carvalho <andredobbss@hotmail.com>
2025-05-19 11:50:53 -0300

    revisando classe Program

--- Program.cs
+++ Program.cs
@@ -12,11 +12,12 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://andredobbss.github.io/FakeDataMaker/") });
 
-#endregion
+    #endregion
 
-# region Congiguration MudBlazor
+    #region Congiguration MudBlazor
 
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
